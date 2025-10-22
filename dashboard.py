897a5e3b19fe2f1d5594ac9a0901a4536e4e2684
Morris-best <kyalomorris774@gmail.com>
2025-10-23 01:50:51 +0300

    dashboard started

--- dashboard.py
+++ dashboard.py
@@ -4,8 +4,28 @@ import streamlit as st
 import pandas as pd 
 import numpy as np
 
-st.header(" Nairobi's Airbnb Market Overview & Investment Analysis")
+# A Dashboard heading and short description
+st.header(" Nairobi's Airbnb Market Overview & Investment Analysis Dashboard 📊")
 
+st.markdown("""
+## Hello 👋! 
+This dashboard, presents a detailed analysis into Nairobi's Airbnb and short-term vacation rentals market, uncovering insights about the distribution, occupancy rates, revenue generation and seasonal trends in the market.
+
+The goal of this analysis is to give you the neccesary information to empower and guide your investment decision and the pricing strategy for your listing. 
+It does this by showcasing key insights, patterns and trends about the listings distribution across Nairobi, their occupancy and revenue generation and trends and patterns in market.
+            
+""")
 data = pd.read_csv(r'https://github.com/Morris-mk/Nairobi_AirbnbMarket_Overview_and-_InvestmentAnalysis/raw/refs/heads/main/data/listings_raw_data.csv')
 
-st.map(data=data, longitude=data.longitude, latitude=data.latitude)
+
+st.checkbox('Show the Listings Data used',
+            st.dataframe(data))
+
+# create the summary analytics tiles 
+col1, col2, col3 = st.columns(3)
+
+
+st.subheader("Distribution of Listings across Nairobi")
+st.map(data=data, width=600, height=400)
+
+
