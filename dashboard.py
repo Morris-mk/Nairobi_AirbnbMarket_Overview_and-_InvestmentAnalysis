67db1cc015c67d104e06b23b8063e1b69bd925bb
Morris-best <kyalomorris774@gmail.com>
2025-10-22 22:41:47 +0300

    analysis

--- dashboard.py
+++ dashboard.py
@@ -4,3 +4,8 @@ import streamlit as st
 import pandas as pd 
 import numpy as np
 
+st.header(" Nairobi's Airbnb Market Overview & Investment Analysis")
+
+data = pd.read_csv(r'https://github.com/Morris-mk/Nairobi_AirbnbMarket_Overview_and-_InvestmentAnalysis/raw/refs/heads/main/data/listings_raw_data.csv')
+
+st.map(data=data, longitude=data.longitude, latitude=data.latitude)
